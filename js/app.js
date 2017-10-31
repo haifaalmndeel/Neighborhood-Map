ec5309ddfefe0f829309179b4b4ba1cf77ce2a00
haifa almndeel <haifaa.almndeel@gmail.com>
2017-10-31 22:44:08 +0300

    Update

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 // Create the map and the infowindow'
 var map, infowindow;
-
 // Create a new blank array for all the listing markers.
 var markers = [];
 // Create locations array to use it any time.
@@ -11,47 +10,47 @@ var locations=[
   {title:"atlantis the blam",location:{lat:25.130443,lng:55.11715}},
   {title:"Dubai Zoo",location:{lat:25.222483,lng:55.256453}},
   {title:"Sky dive",location:{lat:25.091427,lng:55.138296}}];
-
 // Create the  view model for the knockout, search box and markers to make it work.
 // object literal, not a constructor function
 var myViewModel = {
   myLocations: ko.observableArray(locations),
   userInput: ko.observable('')
 };
-
+//create a function when click on one of the list make info window open
+function openInfo(title) {
+  for (var i = 0; i < markers.length; i++) {
+    if (markers[i].title == title)
+      google.maps.event.trigger(markers[i], 'click');
+  }
+}
 myViewModel.mySearch = ko.computed(function () {
   var userInput = this.userInput().toLowerCase();
   var filteredLocations = [];
-
+  //if there is no userInput set all the marker 
   if (!userInput) {
     for (i = 0; i < markers.length; i++) {
       markers[i].setMap(map);
     }
     return this.myLocations();
   } else {
-
+    //push evry marker get in userInput to filteredLocations 
     for (i = 0; i < locations.length; i++) {
-      if (userInput == locations[i].title.toLowerCase()) {
+      if (locations[i].title.toLowerCase().indexOf(userInput) > -1) {
         filteredLocations.push(locations[i]);
       }
     }
     for (i = 0; i < markers.length; i++) {
-      for (j = 0; j < filteredLocations.length; j++) {
+      for (j = 0; j < filteredLocations.length; j++
+      ) {
         if (filteredLocations[j].title == markers[i].getTitle()) {
           markers[i].setMap(map);
         } else {
           markers[i].setMap(null);
         }
       }
-
     }
-
     return filteredLocations;
-
-
-    // filter the list items here
   }
-
 }, myViewModel);
 
 function initMap() {
@@ -63,10 +62,9 @@ function initMap() {
     },
     zoom: 13
   });
-
   infowindow = new google.maps.InfoWindow();
   var bounds = new google.maps.LatLngBounds();
-  
+
   // The following group uses the location array to create an array of markers on initialize.
   locations.forEach(function (d, i) {
     // Get the position from the location array.
@@ -74,40 +72,34 @@ function initMap() {
     var title = locations[i].title;
     // Create a marker per location, and put into markers array.
     var marker = new google.maps.Marker({
-      map: map,
-      position: position,
-      title: title,
-      animation: google.maps.Animation.DROP,
+        map: map,
+        position: position,
+        title: title,
+        animation: google.maps.Animation.DROP,
+        id: i
+      });
 
-      id: i
-    });
     // Push the marker to our array of markers.
     markers.push(marker);
+
     // Create an onclick event to open an infowindow at each marker.
     marker.addListener('click', function () {
-      toggleBounce()
-      populateInfoWindow(this);
-
-    });
-    
+        toggleBounce();
+        populateInfoWindow(this);
+      });
     bounds.extend(markers[i].position);
 
-      function toggleBounce() {
-        if (marker.getAnimation() !== null) {
-          marker.setAnimation(null);
-        }
-         else {
-          marker.setAnimation(google.maps.Animation.BOUNCE);
-        };
-
+    //this function to make the marker animate 
+    function toggleBounce() {
+      if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+      } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
       }
-
-
+    }
   });
   // Extend the boundaries of the map for each marker
   map.fitBounds(bounds);
-
-
 }
 
 // This function populates the infowindow when the marker is clicked. We'll only allow
@@ -121,15 +113,18 @@ function populateInfoWindow(marker) {
     wikiMarker(marker);
     // Make sure the marker property is cleared if the infowindow is closed.
     infowindow.addListener('closeclick', function () {
-      infowindow.setMarker = null;
-    });
+        infowindow.setMarker = null;
+      });
   }
 }
 
 //this function for the third-party wikipedia 
 function wikiMarker(marker) {
   // the API link
-  var wikiURL = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=' + marker.title + '&namespace=0&format=json&callback=cb';
+  var wikiURL =
+    'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=' +
+    marker.title +
+    '&namespace=0&format=json&callback=cb';
   // ajax request 
   $.ajax({
     url: wikiURL,
@@ -145,12 +140,9 @@ function wikiMarker(marker) {
       // to make sure the wikipedia API will work before you click on it 
       infowindow.setContent('<div>' + marker.title + '</div>' + '<div>' + link + '</div>');
       infowindow.open(map, marker);
-
     }
   });
 }
-
-
 
 // this function to open the navbar side .
 function openNav() {
